d08c4ff322efdee20555762c88366cfdee96eb73
jaime martinez romero <jaimemartinezromero@MacBook-Air-de-jaime.local>
2023-08-21 11:27:04 +0200

    añadir input descripcion

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 // Obtener referencias a elementos del DOM
-const taskInput = document.getElementById("taskInput");
-const addTaskButton = document.getElementById("addTaskButton");
-const taskList = document.getElementById("taskList");
+const taskInput = document.getElementById("taskInput"); // Obtener el elemento de entrada de tareas
+const taskDescripcionInput = document.getElementById("taskDescripcionInput"); // Obtener el elemento de entrada de descripción
+const addTaskButton = document.getElementById("addTaskButton"); // Obtener el botón de "Apuntar"
+const taskList = document.getElementById("taskList"); // Obtener la lista de tareas
 
 // Agregar un evento al botón "Apuntar"
 addTaskButton.addEventListener("click", async () => {
   // Obtener el texto de la tarea y limpiar espacios en blanco
   const taskText = taskInput.value.trim();
-  
+  // Obtener la descripción de la tarea
+  const taskDescripcion = taskDescripcionInput.value.trim();
+
   if (taskText !== "") {
     // Enviar la nueva tarea al fake API a través de una solicitud POST
     const response = await fetch("http://localhost:3000/tareas", {
@@ -17,7 +20,7 @@ addTaskButton.addEventListener("click", async () => {
       },
       body: JSON.stringify({
         title: taskText,
-        descripcion: ""
+        descripcion: taskDescripcion, // Incluir la descripción
       }),
     });
 
@@ -27,35 +30,36 @@ addTaskButton.addEventListener("click", async () => {
       addTask(newTask);
     }
 
-    // Limpiar el campo de entrada
+    // Limpiar los campos de entrada
     taskInput.value = "";
+    taskDescripcionInput.value = "";
   }
 });
 
 // Función para agregar una tarea a la lista en la interfaz
 function addTask(task) {
-  const li = document.createElement("li");
+  const li = document.createElement("li"); // Crear un nuevo elemento de lista
   li.innerHTML = `
-    <h1>${task.title}</h1>
-    <h4>${task.descripcion}</h4>
-    <button class="completeButton">Completada ✅</button>
-    <button class="editButton">📝 Editar</button>
-    <button class="deleteButton">🗑️ Eliminar</button>
+    <h1>${task.title}</h1> <!-- Mostrar el título de la tarea -->
+    <h3>${task.descripcion}</h3> <!-- Mostrar la descripción de la tarea -->
+    <button class="completeButton">Completada ✅</button> <!-- Botón de Completada -->
+    <button class="editButton">📝 Editar</button> <!-- Botón de Editar -->
+    <button class="deleteButton" data-taskid="${task.id}">🗑️ Eliminar</button> <!-- Botón de Eliminar -->
   `;
-  taskList.appendChild(li);
+  taskList.appendChild(li); // Agregar la tarea a la lista de tareas en la interfaz
 
-  const completeButton = li.querySelector(".completeButton");
-  const deleteButton = li.querySelector(".deleteButton");
-  const editButton = li.querySelector(".editButton");
+  const completeButton = li.querySelector(".completeButton"); // Obtener el botón Completada
+  const deleteButton = li.querySelector(".deleteButton"); // Obtener el botón Eliminar
+  const editButton = li.querySelector(".editButton"); // Obtener el botón Editar
 
   // Agregar evento al botón "Completada"
   completeButton.addEventListener("click", () => {
-    li.classList.toggle("completed");
+    li.classList.toggle("completed"); // Alternar la clase "completed" para marcar la tarea como completada
   });
 
   // Agregar evento al botón "Eliminar"
   deleteButton.addEventListener("click", async () => {
-    taskList.removeChild(li);
+    taskList.removeChild(li); // Quitar la tarea de la lista en la interfaz
     // Enviar solicitud DELETE al servidor para eliminar la tarea correspondiente
     await fetch(`http://localhost:3000/tareas/${task.id}`, {
       method: "DELETE",
@@ -64,6 +68,7 @@ function addTask(task) {
 
   // Agregar evento al botón "Editar"
   editButton.addEventListener("click", async () => {
+    // Mostrar una ventana emergente para editar el título de la tarea
     const newText = prompt("Editar tarea:", task.title);
     if (newText !== null) {
       // Enviar solicitud PUT al servidor para actualizar el título de la tarea
@@ -79,8 +84,8 @@ function addTask(task) {
       });
 
       if (response.ok) {
-        task.title = newText.trim();
-        li.querySelector("span").textContent = newText.trim();
+        task.title = newText.trim(); // Actualizar el título en el objeto de la tarea
+        li.querySelector("span").textContent = newText.trim(); // Actualizar el título en la interfaz
       }
     }
   });
@@ -88,19 +93,18 @@ function addTask(task) {
 
 // Función para crear y mostrar las tareas en la interfaz
 async function createHtml() {
-  // Limpiar la lista antes de rellenarla
-  taskList.innerHTML = "";
+  taskList.innerHTML = ""; // Limpiar la lista antes de rellenarla
   
   // Obtener tareas del servidor y agregarlas a la lista
   let response = await fetch("http://localhost:3000/tareas");
   let data = await response.json();
 
   data.forEach((task) => {
-    addTask(task);
+    addTask(task); // Agregar cada tarea a la interfaz
   });
 }
 
-// Cargar las tareas al cargar la página
+// Esperar a que se cargue el contenido antes de crear la interfaz
 document.addEventListener("DOMContentLoaded", () => {
-  createHtml();
+  createHtml(); // Llamar a la función para crear y mostrar las tareas
 });
